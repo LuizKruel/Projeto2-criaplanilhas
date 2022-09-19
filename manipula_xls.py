e08b0e79995904db37a44e7ba692c1ff3edf7698
LuizKruel <112526072+LuizKruel@users.noreply.github.com>
2022-09-19 16:28:30 -0300

    Create manipula_xls.py

---
+++ manipula_xls.py
@@ -0,0 +1,22 @@
+"""
+Projeto2-criaplanilhas
+Descrição: módulo que oferece funções para manipular arquivos no formato xls.
+Autora: Luiz Kruel
+Data: 19/09/2022
+Versão: 0.0.1
+"""
+
+# importa pacotes
+
+from openpyxl import workbook
+
+# funções
+
+def cria_xls()-> workbook:
+    '''Esta função cria uma pasta de trabaho MS-Excel.'''
+    pasta = workbook()
+    return pasta
+
+def cria_planilha(nome_planilha: str, pasta: workbook)-> None:
+    pasta.active
+    pasta.create_sheet(nome_planilha)
